c86f6fc1dfc8b389bc83ac75e24080ed31ffa234
jaswanthsurya <jassusai0611@gmail.com>
2019-04-05 10:40:54 +0530

    subscriber code modified and removed db(added file stream)

--- SingleClientAndServer(LocalHost)/TCPSubscriber/TCPSubscriber.cpp
+++ SingleClientAndServer(LocalHost)/TCPSubscriber/TCPSubscriber.cpp
@@ -1,183 +1,12 @@
-
-
 #include<WinSock2.h>
 #include <iostream>
 #include <string>
 #include <windows.h>
-#include <sqlext.h>
-#include <sqltypes.h>
-#include <sql.h>
 #include<sstream>
+#include<fstream>
 
 #pragma warning(disable:4996) 
 using namespace std;
-void showSQLError(unsigned int handleType, const SQLHANDLE& handle)
-
-{
-
-	SQLCHAR SQLState[1024];
-
-	SQLCHAR message[1024];
-
-	if (SQL_SUCCESS == SQLGetDiagRec(handleType, handle, 1, SQLState, NULL, message, 1024, NULL))
-
-		// Returns the current values of multiple fields of a diagnostic record that contains error, warning, and status information
-
-		cout << "SQL driver message: " << message << "\nSQL state: " << SQLState << "." << endl;
-
-}
-
-void checkSQL(char* SQLQuery)
-
-{
-
-	SQLHANDLE SQLEnvHandle = NULL;
-
-	SQLHANDLE SQLConnectionHandle = NULL;
-
-	SQLHANDLE SQLStatementHandle = NULL;
-
-	SQLRETURN retCode = 0;
-
-	do {
-
-		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &SQLEnvHandle))
-
-			// Allocates the environment
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLSetEnvAttr(SQLEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
-
-			// Sets attributes that govern aspects of environments
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, SQLEnvHandle, &SQLConnectionHandle))
-
-			// Allocates the connection
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLSetConnectAttr(SQLConnectionHandle, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0))
-
-			// Sets attributes that govern aspects of connections
-
-			break;
-
-
-
-		SQLCHAR retConString[1024]; // Conection string
-
-		switch (SQLDriverConnect(SQLConnectionHandle, NULL,
-
-			(SQLCHAR*)"DRIVER={SQL Server}; SERVER=LAPTOP-1KKBO5U4; DATABASE=subcriber_table;Trusted=true;",
-
-			SQL_NTS, retConString, 1024, NULL, SQL_DRIVER_NOPROMPT)) {
-
-			// Establishes connections to a driver and a data source
-
-		case SQL_SUCCESS:
-
-			break;
-
-		case SQL_SUCCESS_WITH_INFO:
-
-			break;
-
-		case SQL_NO_DATA_FOUND:
-
-			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
-
-			retCode = -1;
-
-			break;
-
-		case SQL_INVALID_HANDLE:
-
-			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
-
-			retCode = -1;
-
-			break;
-
-		case SQL_ERROR:
-
-			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
-
-			retCode = -1;
-
-			break;
-
-		default:
-
-			break;
-
-		}
-
-
-
-		if (retCode == -1)
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, SQLConnectionHandle, &SQLStatementHandle))
-
-			// Allocates the statement
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLExecDirect(SQLStatementHandle, (SQLCHAR*)SQLQuery, SQL_NTS)) {
-
-			// Executes a preparable statement
-
-			showSQLError(SQL_HANDLE_STMT, SQLStatementHandle);
-
-			break;
-
-		}
-
-		else {
-
-			char name[256];
-
-			while (SQLFetch(SQLStatementHandle) == SQL_SUCCESS) {
-
-				SQLGetData(SQLStatementHandle, 1, SQL_C_DEFAULT, &name, sizeof(name), NULL);
-
-				cout << name << endl;
-
-				// Retrieves data for a single column in the result set
-
-
-
-			}
-
-		}
-
-	} while (FALSE);
-	SQLFreeHandle(SQL_HANDLE_STMT, SQLStatementHandle);
-
-	SQLDisconnect(SQLConnectionHandle);
-
-	SQLFreeHandle(SQL_HANDLE_DBC, SQLConnectionHandle);
-
-	SQLFreeHandle(SQL_HANDLE_ENV, SQLEnvHandle);
-
-	// Frees the resources and disconnects
-
-}
-
 int main()
 {
 		cout << "|.....................TCP SUBSCRIBER.........................|" << endl;
@@ -247,7 +76,9 @@ int main()
 		{
 			cout << "|..............receiving data succedded received.............|" << RecvBuffer << endl;
 		}
-
+		ofstream file;
+		file << RecvBuffer << endl;
+		file.close();
 		//closing socket
 		iCloseSocket = closesocket(TCPClientSocket);
 		if (iCloseSocket == SOCKET_ERROR)
@@ -265,26 +96,6 @@ int main()
 		}
 		else
 			cout << "|...................wsacleanup successful....................|" << endl;
-		if (strlen(RecvBuffer)>=1)
-
-		{
-
-			char SQLQuery1[2000] = "INSERT INTO subsriber(message,date_time) VALUES('";
-
-			strcat(SQLQuery1, RecvBuffer);
-
-			char c[] = "',";
-			strcat(SQLQuery1, c);
-			strcat(SQLQuery1,"CURRENT_TIMESTAMP");
-			strcat(SQLQuery1, ")");
-		
-			checkSQL(SQLQuery1);
-
-			//char SQLQuery2[] = "select * from subsriber";
-
-			//checkSQL(SQLQuery2);
-
-		}
 	system("PAUSE");
 	return 0;
 }
--- SingleClientAndServer(LocalHost)/subscriber_with_DetailsOfPreviousMessages/subscriber_with_DetailsOfPreviousMessages.cpp
+++ SingleClientAndServer(LocalHost)/subscriber_with_DetailsOfPreviousMessages/subscriber_with_DetailsOfPreviousMessages.cpp
@@ -5,11 +5,6 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
-#include <sqlext.h>
-#include <sqltypes.h>
-#include <sql.h>
-#include<sstream>
-
 #include "cgicc/Cgicc.h"
 #include "cgicc/HTTPHTMLHeader.h"
 #include "cgicc/HTMLClasses.h"
@@ -17,194 +12,8 @@
 #pragma warning(disable:4996) 
 using namespace std;
 using namespace cgicc;
-void showSQLError(unsigned int handleType, const SQLHANDLE& handle)
-
-{
-
-	SQLCHAR SQLState[1024];
-
-	SQLCHAR message[1024];
-
-	if (SQL_SUCCESS == SQLGetDiagRec(handleType, handle, 1, SQLState, NULL, message, 1024, NULL))
-
-		// Returns the current values of multiple fields of a diagnostic record that contains error, warning, and status information
-
-		cout << "SQL driver message: " << message << "\nSQL state: " << SQLState << "." << endl;
-
-}
-
-void checkSQL(char* SQLQuery)
-
-{
-
-	SQLHANDLE SQLEnvHandle = NULL;
-
-	SQLHANDLE SQLConnectionHandle = NULL;
-
-	SQLHANDLE SQLStatementHandle = NULL;
-
-	SQLRETURN retCode = 0;
-
-	do {
-
-		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &SQLEnvHandle))
-
-			// Allocates the environment
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLSetEnvAttr(SQLEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
-
-			// Sets attributes that govern aspects of environments
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, SQLEnvHandle, &SQLConnectionHandle))
-
-			// Allocates the connection
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLSetConnectAttr(SQLConnectionHandle, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0))
-
-			// Sets attributes that govern aspects of connections
-
-			break;
-
-
-
-		SQLCHAR retConString[1024]; // Conection string
-
-		switch (SQLDriverConnect(SQLConnectionHandle, NULL,
-
-			(SQLCHAR*)"DRIVER={SQL Server}; SERVER=LAPTOP-1KKBO5U4; DATABASE=subcriber_table;Trusted=true;",
-
-			SQL_NTS, retConString, 1024, NULL, SQL_DRIVER_NOPROMPT)) {
-
-			// Establishes connections to a driver and a data source
-
-		case SQL_SUCCESS:
-
-			break;
-
-		case SQL_SUCCESS_WITH_INFO:
-
-			break;
-
-		case SQL_NO_DATA_FOUND:
-
-			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
-
-			retCode = -1;
-
-			break;
-
-		case SQL_INVALID_HANDLE:
-
-			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
-
-			retCode = -1;
-
-			break;
-
-		case SQL_ERROR:
-
-			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
-
-			retCode = -1;
-
-			break;
-
-		default:
-
-			break;
-
-		}
-
-
-
-		if (retCode == -1)
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, SQLConnectionHandle, &SQLStatementHandle))
-
-			// Allocates the statement
-
-			break;
-
-
-
-		if (SQL_SUCCESS != SQLExecDirect(SQLStatementHandle, (SQLCHAR*)SQLQuery, SQL_NTS)) {
-
-			// Executes a preparable statement
-
-			showSQLError(SQL_HANDLE_STMT, SQLStatementHandle);
-
-			break;
-
-		}
-
-		else {
-
-			char name[256];
-			char time[256];
-			//adding table head
-			cout << "<table class=\"table\">\n"
-				"  <thead class=\"thead-dark\">\n"
-				"    <tr>\n"
-				"      <th scope=\"col\">MESSAGES</th>\n"
-				"      <th scope=\"col\">TIME</th>\n"
-				"    </tr>\n"
-				"  </thead>\n";
-			//table head done
-			cout << "  <tbody>\n";
-
-			while (SQLFetch(SQLStatementHandle) == SQL_SUCCESS) {
-
-				SQLGetData(SQLStatementHandle, 1, SQL_C_DEFAULT, &name, sizeof(name), NULL);
-				SQLGetData(SQLStatementHandle, 2, SQL_C_DEFAULT, &time, sizeof(time), NULL);
-
-				cout << "<tr>\n";
-				cout << "<td>";
-				cout << name;
-				cout << "</td> \n";
-				cout << "<td>";
-				cout << time;
-				cout << "</td> \n";
-				cout << "</tr>";
-			}
-			cout << "  </tbody>\n"
-				"</table>\n";
-				
-
-
 
 			
-
-		}
-
-	} while (FALSE);
-	SQLFreeHandle(SQL_HANDLE_STMT, SQLStatementHandle);
-
-	SQLDisconnect(SQLConnectionHandle);
-
-	SQLFreeHandle(SQL_HANDLE_DBC, SQLConnectionHandle);
-
-	SQLFreeHandle(SQL_HANDLE_ENV, SQLEnvHandle);
-
-	// Frees the resources and disconnects
-
-}
-
 int main(int argc, char **argv)
 {
 	//connect with web page
@@ -230,12 +39,33 @@ int main(int argc, char **argv)
 			"</form>\n"
 			"</body>\n"
 			"</html>";
-
-		// Close the HTML document
+		cout << "<table class=\"table\">\n"
+			"  <thead class=\"thead-dark\">\n"
+			"    <tr>\n"
+			"      <th scope=\"col\">MESSAGES</th>\n"
+			"      <th scope=\"col\">TIME</th>\n"
+			"    </tr>\n"
+			"  </thead>\n";
+		//table head done
+		cout << "  <tbody>\n";
+		string line;
+		ifstream myfile("data.txt");
+		if (myfile.is_open())
+		{
+			while (getline(myfile, line))
+			{
+				cout << "<tr>\n";
+				cout << "<td>";
+				cout << line;
+				cout << "</td> \n";
+				cout << "</tr>";
+			}
+			myfile.close();
+			cout << "  </tbody>\n"
+				"</table>\n";
+		}
+		else cout << "Unable to open file"; 		// Close the HTML document
 		cout << body() << html();
-		char SQLQuery2[] = "select * from subsriber order by date_time desc";
-
-			checkSQL(SQLQuery2);
 	}
 	catch (exception& e) {
 		// handle any errors - omitted for brevity
